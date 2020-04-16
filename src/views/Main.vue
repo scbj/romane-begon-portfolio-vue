fe065f697e9e1a240be5e6b44e8caac6936c37d7
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-04-16 18:40:40 +0200

    Prestation preview
    ◽ Import prestations data
    ◽ Update router to use <Main>
    ◽ Create Prestation component

---
+++ src/views/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="main">
+    <template v-for="(page, index) in prestations">
+      <Prestation
+        :key="index"
+        class="main__section"
+        :title="page.title"
+        :description="page.description"
+        :background-image="page.backgroundImage"
+      />
+    </template>
+  </main>
+</template>
+
+<script>
+import Prestation from '@/views/Prestation.vue'
+import prestations from '@/assets/data/prestations.json'
+
+export default {
+  components: {
+    Prestation
+  },
+
+  computed: {
+    prestations () {
+      return prestations
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main__section {
+  height: 100vh;
+}
+</style>
---
+++ src/views/Prestation.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="prestation">
+    <h2 class="prestation__title">
+      {{ title }}
+    </h2>
+    <p class="prestation__description">
+      {{ description }}
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    backgroundImage: {
+      type: String,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      prestationCount: 7,
+      prestationNumber: 2
+    }
+  },
+
+  created () {
+    // To use counter :
+    // registerPrestationToTheStore()
+  },
+
+  beforeDestroy () {
+    // unregisterPrestationToTheStore()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.prestation {
+  padding: 60px;
+}
+
+.prestation__title {
+  font-family: "Orpheus Pro", serif;
+  font-weight: 700;
+  font-size: 127px;
+  letter-spacing: 3.68px;
+}
+
+.prestation__description {
+  font-family: 'TT Commons';
+  font-weight: 400;
+  font-size: 16px;
+  letter-spacing: 0.3px;
+}
+</style>
