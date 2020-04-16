c95949734002b87a20b7abbc9f6bc254e8e8cc33
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-04-16 18:39:07 +0200

    Configure ESLint, JS & base CSS
    ◽ Add ESLint rules
    ◽ Add jsconfig to handle '@' alias
    ◽ Import fonts (TT Commons & Orpheus Pro)
    ◽ Create base CSS & font CSS

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/standard'
   ],
   parserOptions: {
@@ -12,6 +12,13 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': [2, {
+      'singleline': 2,
+      'multiline': {
+        'max': 1,
+        'allowFirstLine': false
+      }
+    }]
   }
 }
