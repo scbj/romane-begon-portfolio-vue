1afa2141f71f43bf7da4cab7f2b020e3ae579ebb
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-04-17 16:59:56 +0200

    PrestationCounter
    ◽ Update backgroundImage urls
    ◽ Remove HelloWorld & Home
    ◽ Create <PrestationCounter>

---
+++ src/components/PrestationCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="prestation-counter">
+    <span class="prestation-counter__actual">
+      0{{ 2 }}
+    </span>
+    <span class="prestation-counter__total">
+      0{{ 7 }}
+    </span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Prestation.vue
+++ src/views/Prestation.vue
@@ -1,5 +1,12 @@
 <template>
   <section class="prestation">
+    <img
+      :src="backgroundImage"
+      alt="background image"
+      class="prestation__background"
+    >
+    <div class="prestation__overlay" />
+    <PrestationCounter class="prestation__counter" />
     <h2 class="prestation__title">
       {{ title }}
     </h2>
@@ -10,7 +17,13 @@
 </template>
 
 <script>
+import PrestationCounter from '@/components/PrestationCounter'
+
 export default {
+  components: {
+    PrestationCounter
+  },
+
   props: {
     title: {
       type: String,
@@ -46,7 +59,12 @@ export default {
 
 <style lang="scss" scoped>
 .prestation {
-  padding: 60px;
+  color: #f3f3f3;
+  display: grid;
+  grid-template-rows: 1fr repeat(4, auto) 1fr;
+  grid-template-columns: 433px 1fr;
+  justify-items: flex-start;
+  text-align: left;
 }
 
 .prestation__title {
@@ -54,6 +72,9 @@ export default {
   font-weight: 700;
   font-size: 127px;
   letter-spacing: 3.68px;
+  line-height: 2em;
+  grid-row: 3 / 4;
+  grid-column: 2 / 3;
 }
 
 .prestation__description {
@@ -61,5 +82,28 @@ export default {
   font-weight: 400;
   font-size: 16px;
   letter-spacing: 0.3px;
+  line-height: 0.512px;
+  grid-row: 4 / 5;
+  grid-column: 2 / 3;
+  max-width: 428px;
+}
+
+.prestation__background {
+  width: 100%;
+  grid-row: 1 / -1;
+  grid-column: 1 / -1;
+  background-size: cover;
+}
+
+.prestation__counter {
+  grid-row: 2 / 3;
+  grid-column: 2 / 3;
+}
+
+.prestation__overlay {
+  width: 100%;
+  grid-row: 1 / -1;
+  grid-column: 1 / -1;
+  background: linear-gradient(90deg, rgba(15,14,11,0.96) 10%, rgba(15,14,11,0.06) 56%);;
 }
 </style>
