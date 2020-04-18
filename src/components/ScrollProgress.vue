80bc2de74c0c26f447d20a126262440428ca4b14
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-04-18 21:24:56 +0200

    Scroll Progress component
    ◽ Import Garamond font
    ◽ Create <ScrollProgress>
    ◽ Create Weddings page

---
+++ src/components/ScrollProgress.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="scroll-progress" :style="cssVariables">
+    <div class="scroll-progress__indicator-background" />
+    <div class="scroll-progress__indicator" />
+
+    <template v-for="(page, index) in pages">
+      <!-- Page numbers -->
+      <span :key="'index' + index" class="scroll-progress__index">
+        0{{ index + 1 }}
+      </span>
+
+      <!-- Labels -->
+      <span
+        :key="'label-' + index"
+        class="scroll-progress__label"
+      >
+        {{ page.toUpperCase() }}
+      </span>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeIndex: 0
+    }
+  },
+
+  computed: {
+    pages: () => [
+      'Accueil',
+      'Mariages',
+      'Portrait',
+      'Familles &\n Couples',
+      'A propos',
+      'Contact',
+      'Espace\n Client'
+    ],
+
+    cssVariables () {
+      return {
+        '--row-count': this.pages.length
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.scroll-progress {
+  font-family: "Garamond Premier Pro Display", serif;
+  font-weight: 300;
+  font-size: 16px;
+  letter-spacing: 1.38px;
+  line-height: 23px;
+  white-space: pre;
+
+  display: inline-grid;
+  grid-template-columns: repeat(3, auto);
+  grid-auto-rows: auto;
+  column-gap: 28px;
+}
+
+.scroll-progress__indicator,
+.scroll-progress__indicator-background {
+  grid-column: 2 / 3;
+  grid-row: 1 / calc(var(--row-count) + 1);
+  width: 2px;
+}
+
+.scroll-progress__indicator {
+  background: black;
+  transform: scaleY(0.2);
+  transform-origin: 50% 0%;
+}
+
+.scroll-progress__indicator-background {
+  background: rgb(226, 226, 226);
+}
+
+.scroll-progress__label {
+  text-align: left;
+  margin-bottom: 82px;
+  cursor: pointer;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.scroll-progress__index {
+  text-align: right;
+}
+</style>
---
+++ src/views/Weddings.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="weddings">
+    <ScrollProgress />
+  </div>
+</template>
+
+<script>
+import ScrollProgress from '@/components/ScrollProgress'
+
+export default {
+  components: {
+    ScrollProgress
+  }
+}
+</script>
