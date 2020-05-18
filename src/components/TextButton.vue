117c91646058342d2f809cfabd87cc14133740f2
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-05-18 23:07:42 +0200

    Create <TextButton> component

---
+++ src/components/TextButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="text-button">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.text-button {
+  font-size: 14px;
+  font-weight: 500;
+  font-family: "TT Commons", sans-serif;
+  line-height: 32px;
+  letter-spacing: 3.92px;
+}
+</style>
