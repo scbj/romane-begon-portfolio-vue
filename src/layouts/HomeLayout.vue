43d70eb1bcac56d26ead5ce76af6e710a66561f2
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-05-30 02:45:31 +0200

    Create <HomeLayout>

---
+++ src/layouts/HomeLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home-layout">
+    <ParallaxContainer class="home-layout__content" tag="main">
+      <slot name="content" />
+    </ParallaxContainer>
+    <div class="home-layout__fixed">
+      <slot name="fixed" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ParallaxContainer from '@/components/ParallaxContainer'
+
+export default {
+  components: {
+    ParallaxContainer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_mixins.scss';
+@import '@/assets/styles/_vars.scss';
+
+.home-layout {
+  height: 100%;
+}
+
+.home-layout__content {
+  @include hide-scrollbar;
+}
+
+.home-layout__fixed {
+  position: fixed;
+  right: 5rem;
+  top: 14rem;
+  z-index: 10;
+
+  @media screen and (min-width: $extraLarge) {
+    right: 68px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,36 @@
 <template>
-  <ParallaxContainer class="home" tag="main">
-    <StartupParallax
-      v-for="(n, index) of 4"
-      :key="index"
-      :style="`z-index: ${n}`"
-    />
-  </ParallaxContainer>
+  <HomeLayout>
+    <template v-slot:content>
+      <StartupParallax
+        v-for="(n, index) of 4"
+        :key="index"
+        :style="`z-index: ${n}`"
+      />
+    </template>
+    <template v-slot:fixed>
+      <SocialLinks orientation="vertical" />
+    </template>
+  </HomeLayout>
 </template>
 
 <script>
+import HomeLayout from '@/layouts/HomeLayout'
+import SocialLinks from '@/components/SocialLinks'
 import StartupParallax from '@/views/StartupParallax'
-import ParallaxContainer from '@/components/ParallaxContainer'
 
 export default {
   components: {
-    ParallaxContainer,
+    HomeLayout,
+    SocialLinks,
     StartupParallax
   }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/_mixins.scss';
-
-.home {
-  @include hide-scrollbar;
+.home__social {
+  background: red;
+  height: 160px;
+  width: 30px;
 }
 </style>
