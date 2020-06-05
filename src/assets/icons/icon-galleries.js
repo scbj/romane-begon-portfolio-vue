594dbef4847228cccbd6834eb7e37b8d18d4c339
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-06-05 03:05:08 +0200

    Import galleries icon

---
+++ src/assets/icons/icon-galleries.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+var icon = require('vue-svgicon')
+icon.register({
+  'icon-galleries': {
+    width: 18,
+    height: 18,
+    viewBox: '0 0 18 18',
+    data: '<g _fill="#fff"><path pid="0" d="M1 0h2a1 1 0 011 1v2a1 1 0 01-1 1H1a1 1 0 01-1-1V1a1 1 0 011-1zM8 0h2a1 1 0 011 1v2a1 1 0 01-1 1H8a1 1 0 01-1-1V1a1 1 0 011-1zM15 0h2a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1V1a1 1 0 011-1zM1 7h2a1 1 0 011 1v2a1 1 0 01-1 1H1a1 1 0 01-1-1V8a1 1 0 011-1zM8 7h2a1 1 0 011 1v2a1 1 0 01-1 1H8a1 1 0 01-1-1V8a1 1 0 011-1zM15 7h2a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1V8a1 1 0 011-1zM1 14h2a1 1 0 011 1v2a1 1 0 01-1 1H1a1 1 0 01-1-1v-2a1 1 0 011-1zM8 14h2a1 1 0 011 1v2a1 1 0 01-1 1H8a1 1 0 01-1-1v-2a1 1 0 011-1zM15 14h2a1 1 0 011 1v2a1 1 0 01-1 1h-2a1 1 0 01-1-1v-2a1 1 0 011-1z"/></g>'
+  }
+})
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 require('./icon-cross')
 require('./icon-facebook')
+require('./icon-galleries')
 require('./icon-instagram')
 require('./icon-menu')
 require('./icon-pinterest')
