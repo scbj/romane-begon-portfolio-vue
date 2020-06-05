e49a9dc16349196f2392c6e72fd9dd54aa228f13
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-06-05 12:45:43 +0200

    Refactor <BaseIcon> to functional component

---
+++ src/components/BaseIcon.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'BaseIcon',
+  functional: true,
+
+  props: {
+    color: {
+      type: String,
+      default: '#fff'
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: '20'
+    }
+  },
+
+  render (h, { props, data, listeners }) {
+    return (
+      <SvgIcon
+        class="base-icon"
+        icon={`icon-${props.name}`}
+        color={props.color}
+        height={props.size}
+        width={props.width}
+        {...data} {...listeners}
+      />
+    )
+  }
+}
