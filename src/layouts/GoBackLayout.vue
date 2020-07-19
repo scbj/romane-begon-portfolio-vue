7b2d2ab1997411ea4791260cf4d4608dba77acfc
Sacha Bejaud <sacha.bejaud@scbj.io>
2020-07-19 20:15:56 +0200

    feat: create prestation page

---
+++ src/layouts/GoBackLayout.vue
@@ -0,0 +1,70 @@
+<template>
+  <ThemeStyle mode="light">
+    <div
+      slot-scope="{ theme }"
+      class="go-back-layout"
+      :style="theme"
+    >
+      <header class="go-back-layout__header">
+        <BaseButton
+          class="go-back-layout__button"
+          :color="theme['--text-color']"
+          icon="cross"
+          :icon-scale="2"
+          :icon-fill="false"
+          @click="goBack"
+        />
+        <div class="go-back-layout__header-content">
+          <slot name="header" />
+        </div>
+      </header>
+      <div class="go-back-layout__content">
+        <slot />
+      </div>
+    </div>
+  </ThemeStyle>
+</template>
+
+<script>
+import ThemeStyle from '@/components/ThemeStyle'
+
+export default {
+  components: {
+    ThemeStyle
+  },
+
+  mounted () {
+    this.$store.set('ui/theme@mode', 'light')
+  },
+
+  methods: {
+    goBack () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.go-back-layout {
+  background: white;
+  min-height: 100%;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.go-back-layout__header {
+  background: rgba(white, 0.5);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5rem;
+  margin-right: 5rem;
+}
+
+.go-back-layout__content {
+  grid-column: 1 / 3;
+  grid-row: 1 / 3;
+}
+</style>
--- src/views/Prestation.vue
+++ src/views/Prestation.vue
@@ -1,109 +1,45 @@
 <template>
-  <section class="prestation">
-    <img
-      :src="backgroundImage"
-      alt="background image"
-      class="prestation__background"
-    >
-    <div class="prestation__overlay" />
-    <PrestationCounter class="prestation__counter" />
-    <h2 class="prestation__title">
-      {{ title }}
-    </h2>
-    <p class="prestation__description">
-      {{ description }}
-    </p>
-  </section>
+  <GoBackLayout class="prestation">
+    <template #header>
+      <RouterLink :to="{ name: 'prestationInfos' }">
+        Infos
+      </RouterLink>
+      <RouterLink :to="{ name: 'prestationGallery' }">
+        Galerie
+      </RouterLink>
+    </template>
+    <RouterView />
+  </GoBackLayout>
 </template>
 
 <script>
-import PrestationCounter from '@/components/PrestationCounter'
+import GoBackLayout from '@/layouts/GoBackLayout'
+
+function routeGuard (to, from, next) {
+  const allowed = ['mariages', 'portraits', 'familles-couples']
+  if (allowed.includes(to.params.prestation)) {
+    return next()
+  }
+  next({ name: 'home' })
+}
 
 export default {
   components: {
-    PrestationCounter
+    GoBackLayout
   },
 
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    description: {
-      type: String,
-      required: true
-    },
-    backgroundImage: {
-      type: String,
-      required: true
-    }
-  },
-
-  data () {
-    return {
-      prestationCount: 7,
-      prestationNumber: 2
-    }
-  },
-
-  created () {
-    // To use counter :
-    // registerPrestationToTheStore()
-  },
-
-  beforeDestroy () {
-    // unregisterPrestationToTheStore()
-  }
+  beforeRouteUpdate: routeGuard,
+  beforeRouteEnter: routeGuard
 }
 </script>
 
 <style lang="scss" scoped>
 .prestation {
-  color: #f3f3f3;
-  display: grid;
-  grid-template-rows: 1fr repeat(4, auto) 1fr;
-  grid-template-columns: 433px 1fr;
-  justify-items: flex-start;
-  text-align: left;
+  display: flex;
+  flex-direction: column;
 }
 
-.prestation__title {
-  font-family: "Orpheus Pro", serif;
-  font-weight: 700;
-  font-size: 127px;
-  letter-spacing: 3.68px;
-  line-height: 2em;
-  grid-row: 3 / 4;
-  grid-column: 2 / 3;
-}
-
-.prestation__description {
-  font-family: 'TT Commons';
-  font-weight: 400;
-  font-size: 16px;
-  letter-spacing: 0.3px;
-  line-height: 0.512px;
-  grid-row: 4 / 5;
-  grid-column: 2 / 3;
-  max-width: 428px;
-}
-
-.prestation__background {
-  width: 100%;
-  grid-row: 1 / -1;
-  grid-column: 1 / -1;
-  background-size: cover;
-}
-
-.prestation__counter {
-  grid-row: 2 / 3;
-  grid-column: 2 / 3;
-}
-
-.prestation__overlay {
-  width: 100%;
-  grid-row: 1 / -1;
-  grid-column: 1 / -1;
-  background: linear-gradient(90deg, rgba(15,14,11,0.96) 10%, rgba(15,14,11,0.06) 56%);;
+.prestation__header {
+  align-self: flex-end;
 }
 </style>
