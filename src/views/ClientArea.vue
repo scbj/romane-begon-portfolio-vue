4fbe1cccc84f296803af16efe0ad268cddaeeec3
Sacha Bejaud <sacha.bejaud@scbj.io>
2020-07-19 21:19:14 +0200

    feat: make a grid from galleries in client area

--- src/views/ClientArea.vue
+++ src/views/ClientArea.vue
@@ -1,34 +1,38 @@
 <template>
-  <ThemeStyle mode="light">
-    <div
-      slot-scope="{ theme }"
-      class="client-area"
-      :style="theme"
-    >
-      <TextTitle extra-large class="client-area__title">
-        Espace Client
-      </TextTitle>
-      <TextParagraph>
-        Je vous ai accompagné dans les belles rues de Toulouse pour une séance photo inoubliable
-        ? Je vous ai suivi dans un lieu magique pour immortaliser votre histoire ? J’ai passé l’une des
-        plus belles journées de votre vie en votre compagnie ?
-      </TextParagraph>
-      <TextParagraph>
-        Bienvenue dans votre espace personnel, il ne vous reste plus qu’à entrer votre mot de passe
-        personnel pour frissonner d’émotions, revoir les sourires de vos proches, admirer les
-        empreintes du bonheur ou revivre le plus beau jour de votre vie
-      </TextParagraph>
-      <ListView class="client-area-preview__galleries" :items="galleries">
-        <template v-slot:item="{ item }">
-          <LumysGallery :gallery="item" />
-        </template>
-      </ListView>
-    </div>
-  </ThemeStyle>
+  <GoBackLayout>
+    <ThemeStyle mode="light">
+      <div
+        slot-scope="{ theme }"
+        class="client-area"
+        :style="theme"
+      >
+        <TextTitle large class="client-area__title">
+          Espace Client
+        </TextTitle>
+        <TextParagraph>
+          Je vous ai accompagné dans les belles rues de Toulouse pour une séance photo inoubliable
+          ? Je vous ai suivi dans un lieu magique pour immortaliser votre histoire ? J’ai passé l’une des
+          plus belles journées de votre vie en votre compagnie ?
+        </TextParagraph>
+        <TextParagraph>
+          Bienvenue dans votre espace personnel, il ne vous reste plus qu’à entrer votre mot de passe
+          personnel pour frissonner d’émotions, revoir les sourires de vos proches, admirer les
+          empreintes du bonheur ou revivre le plus beau jour de votre vie
+        </TextParagraph>
+        <ListView class="client-area__galleries" :items="galleries">
+          <template v-slot:item="{ item }">
+            <LumysGallery :gallery="item" />
+          </template>
+        </ListView>
+      </div>
+    </ThemeStyle>
+  </GoBackLayout>
 </template>
 
 <script>
 import { call, get } from 'vuex-pathify'
+
+import GoBackLayout from '@/layouts/GoBackLayout'
 
 import ListView from '@/components/ListView'
 import LumysGallery from '@/components/LumysGallery'
@@ -38,6 +42,7 @@ import ThemeStyle from '@/components/ThemeStyle'
 
 export default {
   components: {
+    GoBackLayout,
     ListView,
     LumysGallery,
     TextParagraph,
@@ -65,5 +70,12 @@ export default {
   background: white;
   min-height: 100vh;
   padding: 4rem;
+}
+
+.client-area__galleries {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  gap: 3rem;
+  margin-top: 4rem;
 }
 </style>
