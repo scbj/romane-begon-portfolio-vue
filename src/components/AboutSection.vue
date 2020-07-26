084c83b4dde8ecc02c8d71b735517eba71811a6b
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-07-27 00:04:50 +0200

    feat: create temporary about section

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <Intersect :treshold="[0.1, 0.5, 0.9]" @enter="onEnter">
+    <ParallaxGroup class="about-section" />
+  </Intersect>
+</template>
+
+<script>
+import Intersect from 'vue-intersect'
+
+import ParallaxGroup from '@/components/ParallaxGroup'
+
+export default {
+  components: {
+    Intersect,
+    ParallaxGroup
+  },
+
+  methods: {
+    onEnter (entry) {
+      if (entry[0].intersectionRatio > 0.2) {
+        this.$emit('visible')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.about-section {
+  background: white;
+  min-height: 100vh;
+}
+</style>
