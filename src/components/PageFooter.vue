a681c9aa34f648688cc1b1bfceced61400f90f88
Sacha Bejaud <sacha.bejaud@outlook.com>
2020-08-19 18:42:35 +0200

    feat: create page header and footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="page-footer">
+    <SocialLinks
+      class="page-footer__social"
+      color="#1C1C1C"
+      :scale="1"
+    />
+    <CopyrightStatement class="page-footer__copyright" />
+  </footer>
+</template>
+
+<script>
+import CopyrightStatement from '@/components/CopyrightStatement'
+import SocialLinks from '@/components/SocialLinks'
+
+export default {
+  components: {
+    CopyrightStatement,
+    SocialLinks
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-footer {
+  background: rgba(black, 0.05);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3rem;
+  margin-top: 4rem;
+}
+
+.page-footer__copyright {
+  color: #1C1C1C;
+  text-align: center;
+  font-size: 1rem;
+  line-height: 1.4em;
+
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="page-header">
+    <BaseButton
+      class="page-header__button"
+      color="#0f0e0b"
+      icon="cross"
+      :icon-scale="1.111"
+      :icon-fill="false"
+      @click="goBack"
+    />
+    <div class="page-header__content">
+      <slot />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack () {
+      this.$router.push({ name: 'home' })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_vars.scss';
+
+.page-header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+  padding-right: 5rem;
+
+  z-index: 1;
+
+  @media screen and (min-width: $extraLarge) {
+    background: transparent;
+    padding: 68px;
+    padding-right: 134px;
+  }
+}
+
+.page-header__content {
+  display: flex;
+}
+</style>
